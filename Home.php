ef7b39af93844282ffb387a30e5aaaf6f34122f9
marjaygab <marjay_gabriel_tapay@dlsl.edu.ph>
2018-10-06 19:38:36 +0800

    Changed storage from MultiDimensional to Arrayed Objects

--- Home.php
+++ Home.php
@@ -1,3 +1,7 @@
+<?php
+include 'Job.php';
+
+ ?>
 <html>
 <head>
 <title>Multilevel Queue</title>
@@ -38,7 +42,7 @@
         </tr>
             <?php
                 if(isset($_POST['upload'])){
-                    
+
                     function getLeastAT($main_table){
                         for($i=0 ; $i < count($main_table) ; $i++){
                             $temparray[$i] = $main_table[$i]['AT'];
@@ -46,70 +50,67 @@
                         sort($temparray);
                         return $temparray[0];
                     }
-                    
+
                      //For loop to display the stored values.
-                    function displayValues($copy_table,$main_table){
-                          for($i=0;$i<count($copy_table);$i++){
+                    function displayValues($JOB_LIST){
+                          for($i=0;$i<count($JOB_LIST);$i++){
                         echo "<tr>";
                             echo "<td>";
-                            echo $copy_table[$i]['PID'];
+                            echo $JOB_LIST[$i]->JOB_ID;
                             echo "</td>";
                         echo "<td>";
-                            echo $copy_table[$i]['AT'];
+                            echo $JOB_LIST[$i]->AT;
                             echo "</td>";
                         echo "<td>";
-                            echo $copy_table[$i]['PRIORITY'];
+                            echo $JOB_LIST[$i]->PRIORITY;;
                             echo "</td>";
                         echo "<td>";
-                            echo $copy_table[$i]['MEMORY'] . ' KB';
+                            echo $JOB_LIST[$i]->MEMORY . ' KB';
                             echo "</td>";
                         echo "<td>";
-                            echo $copy_table[$i]['BT'];
+                            echo $JOB_LIST[$i]->BT;;
                             echo "</td>";
                         echo "<td>";
-                            echo $main_table[$i]['FT'];
+                            echo $JOB_LIST[$i]->FT;;
                             echo "</td>";
                         echo "</tr>";
                     }
                     }
-                    
-                    
+
+
             //Code for uploading to $_SERVER['DOCUMENT_ROOT']
             if($_FILES["data-file"]["error"] >0){
-            echo "Error:" . $_FILES["data-file"]["error"] . "<br />";
+              echo "Error:" . $_FILES["data-file"]["error"] . "<br />";
             }
             else{
-               $uploaddir = $_SERVER['DOCUMENT_ROOT'] . '/';
-                $uploadfile = $uploaddir . basename($_FILES['data-file']['name']);
-                $filename = $_FILES["data-file"]["name"];
-                 move_uploaded_file($_FILES["data-file"]["tmp_name"],$uploadfile);
-            }    
-            
+              $uploaddir = $_SERVER['DOCUMENT_ROOT'] . '/';
+              $uploadfile = $uploaddir . basename($_FILES['data-file']['name']);
+              $filename = $_FILES["data-file"]["name"];
+              move_uploaded_file($_FILES["data-file"]["tmp_name"],$uploadfile);
+            }
+
             $myfile = fopen($uploadfile,"r") or die("Unable to open file!");
             $tempnum = 0;
             $tempindex = 0;
-                    
+
+
+
+            $JOB_LIST = array();
             //While loop for getting table values and inserting it to main_table multidimensional array.
             while(!feof($myfile)){
                 $line = fgets($myfile);
                 preg_match_all("/<(.*?)>/", $line,$matches);
                 if($tempnum!==0){
-                    $copy_table[$tempindex]["PID"] = $main_table[$tempindex]["PID"] = $matches[1][0];
-                    $copy_table[$tempindex]["AT"] = $main_table[$tempindex]["AT"] = $matches[1][1];
-                    $copy_table[$tempindex]["PRIORITY"] = $main_table[$tempindex]["PRIORITY"] = $matches[1][2];
-                    $copy_table[$tempindex]["MEMORY"] = $main_table[$tempindex]["MEMORY"] = $matches[1][3];
-                    $copy_table[$tempindex]["BT"] = $main_table[$tempindex]["BT"] = $matches[1][4];
+                    $temp = new Job($matches[1][0],$matches[1][1],$matches[1][2],$matches[1][3],$matches[1][4]);
+                    array_push($JOB_LIST,$temp);
                     $main_table[$tempindex]["FT"] = " ";
                     $tempindex++;
                 }
-                $tempnum++;   
+                $tempnum++;
             }
             fclose($myfile);
-                   
-            displayValues($copy_table,$main_table);
-                    
-            
-                    
+
+            displayValues($JOB_LIST);
             }
             ?>
     </table>
---
+++ Job.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ *
+ */
+class Job
+{
+  public $JOB_ID;
+  public $AT;
+  public $BT;
+  public $PRIORITY;
+  public $MEMORY;
+  public $FT=0;
+
+  function __construct($JOB_ID_VAL,$AT_VAL,$PRIO,$MEM,$BT_VAL)
+  {
+    # code...
+    $this->JOB_ID = $JOB_ID_VAL;
+    $this->AT = $AT_VAL;
+    $this->BT = $BT_VAL;
+    $this->PRIORITY = $PRIO;
+    $this->MEMORY = $MEM;
+
+  }
+}
+
+
+
+?>
